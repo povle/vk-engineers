3e8fda68d4be60d8c3c149a1a1b0ed6e1aa95321
povle <pasha@blinov.co>
2020-10-24 23:20:51 +0300

    index last_name and group

--- user.py
+++ user.py
@@ -6,9 +6,9 @@ user_db = pw.SqliteDatabase(config.user_db_path)
 
 class User(pw.Model):
     vk_id = pw.CharField(primary_key=True)
-    group = pw.CharField(null=True)
+    group = pw.CharField(null=True, index=True)
     first_name = pw.CharField()
-    last_name = pw.CharField()
+    last_name = pw.CharField(index=True)
     state = pw.CharField(default=states.USER_NEW)
     state_context = pw.CharField(null=True)
 
