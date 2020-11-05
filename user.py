66a1abea24e6223067f8db317cc7e338df28c37f
povle <pasha@blinov.co>
2020-11-05 13:36:38 +0300

    migrate to mysql

--- user.py
+++ user.py
@@ -2,10 +2,10 @@ import peewee as pw
 import config
 import states
 
-user_db = pw.SqliteDatabase(config.user_db_path, pragmas=[('journal_mode', 'wal')])
+user_db = pw.MySQLDatabase(config.user_db_path)
 
 class User(pw.Model):
-    vk_id = pw.CharField(primary_key=True)
+    vk_id = pw.CharField(primary_key=True, max_length=128)
     group = pw.CharField(null=True, index=True)
     first_name = pw.CharField()
     last_name = pw.CharField(index=True)
